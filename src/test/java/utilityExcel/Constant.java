9d9686fb6ddabcad62a6d345cd4f0ea02f9f5c6b
Leonardo de Carvalho Sonoda <leonardo.sonoda@rsinet.com.br>
2020-03-26 17:12:49 -0300

    Instalado dependencias do ApocherPoi e utilitys do excel.

---
+++ src/test/java/utilityExcel/Constant.java
@@ -0,0 +1,21 @@
+package utilityExcel;
+
+public class Constant {
+	
+	//public static final String URL = "http://www.store.demoqa.com";
+
+    //public static final String Username = "testuser_1";
+
+    //public static final String Password = "Test@123";
+    
+    
+    
+    
+
+	//Caminho da pasta do arquivo do excel
+	public static final String Path_TestData = "C:\\Users\\leonardo.sonoda\\Documents\\" ;
+	//Nome do arquivo excel
+	public static final String File_TestData = "exemplo.xlsx" ;
+
+
+}
